781aa61baeba058c5ed7edd8a9f93ef3819bf57d
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-11-29 11:31:24 +0530

    feat: remove unused SVG files and clean up imports in Home component

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,4 @@
-"use client";
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
+"use client"; 
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
