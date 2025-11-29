4a0490592f3d84301f5f35a2b447cd2d37283e6d
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-11-29 11:31:42 +0530

    feat: update middleware for authentication handling and configure image domains

--- middleware.js
+++ middleware.js
@@ -3,11 +3,14 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export async function middleware(request) {
   const { isAuthenticated } = getKindeServerSession();
+
   if (!(await isAuthenticated())) {
     return NextResponse.redirect(
       new URL("/api/auth/login?post_login_redirect_url=/dashboard", request.url)
     );
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
