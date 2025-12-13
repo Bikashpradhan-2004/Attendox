c19cb2a966e259243bcf943fd5e56fc76d98bde4
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-12-14 00:25:14 +0530

    feat: define database schema for grades, students, and attendance

---
+++ utils/schema.js
@@ -0,0 +1,28 @@
+import {
+  integer,
+  pgTable,
+  serial,
+  varchar,
+  boolean,
+} from "drizzle-orm/pg-core";
+
+export const GRADES = pgTable("grades", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  grade: varchar("grade", { length: 10 }).notNull(),
+});
+
+export const STUDENTS = pgTable("students", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 20 }).notNull(),
+  grade: varchar("grade", { length: 10 }).notNull(),
+  address: varchar("address", { length: 50 }),
+  contact: varchar("contact", { length: 11 }),
+});
+
+export const ATTENDANCE = pgTable("attendance", {
+  id: serial("id").primaryKey(),
+  studentId: integer("student_id").notNull(),
+  present: boolean("present").default(false),
+  day: integer("day").notNull(),
+  date: varchar("date", { length: 20 }).notNull(),
+});
