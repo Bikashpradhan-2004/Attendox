f84a952c4e599f12cd7781de6024664a83e4e3c6
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-12-14 00:30:09 +0530

    feat: add useTableData hook for managing table state and search

---
+++ hooks/useTableData.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const useTableData = (initialData = []) => {
+  const [data, setData] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    setData(initialData);
+  }, [initialData]);
+
+  const handleSearch = (value) => {
+    setSearchText(value);
+  };
+
+  const resetSearch = () => {
+    setSearchText("");
+  };
+
+  return {
+    data,
+    setData,
+    searchText,
+    handleSearch,
+    resetSearch,
+  };
+};
+
+export default useTableData;
+
+// File path: app/lib/hooks/useTableData.js
