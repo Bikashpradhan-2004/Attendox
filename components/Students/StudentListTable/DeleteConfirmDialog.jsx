e104de1eb8b6164fb8b35237e30f97e63818e808
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-12-14 00:34:11 +0530

    feat: create DeleteConfirmDialog component using AlertDialog

---
+++ components/Students/StudentListTable/DeleteConfirmDialog.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+const DeleteConfirmDialog = ({
+  onConfirm,
+  title = "Are you absolutely sure?",
+  description = "This action cannot be undone. This will permanently delete this record.",
+  triggerButton,
+}) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{triggerButton}</AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default DeleteConfirmDialog;
