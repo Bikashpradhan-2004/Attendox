c53c0e62a7a8bf67571f52b496f5a60e59df224e
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-12-16 11:46:52 +0530

    feat(attendance): add month selector to attendance page

---
+++ app/dashboard/attendance/page.jsx
@@ -0,0 +1,12 @@
+import MonthSelection from "@/components/Attendance/MonthSelection";
+
+const Attendance = () => {
+  return (
+    <div className="p-7">
+      <h1 className="text-2xl font-bold">Attendance</h1>
+      <MonthSelection />
+    </div>
+  );
+};
+
+export default Attendance;
