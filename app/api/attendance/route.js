6b4f7136b503821023375dbc96f5422a3689cde1
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-12-20 00:53:40 +0530

    feat: add GET API to fetch student attendance by grade and month

---
+++ app/api/attendance/route.js
@@ -0,0 +1,31 @@
+import { db } from "@/utils";
+import { ATTENDANCE, STUDENTS } from "@/utils/schema";
+import { NextResponse } from "next/server";
+import { eq, and, or, isNull } from "drizzle-orm";
+
+export async function GET(req) {
+  const searchParams = req.nextUrl.searchParams;
+  const grade = searchParams.get("grade");
+  const month = searchParams.get("month");
+
+  const result = await db
+    .select({
+      name: STUDENTS.name,
+      present: ATTENDANCE.present,
+      day: ATTENDANCE.day,
+      date: ATTENDANCE.date,
+      grade: STUDENTS.grade,
+      studentId: STUDENTS.id,
+      attendanceId: ATTENDANCE.id,
+    })
+    .from(STUDENTS)
+    .leftJoin(ATTENDANCE, eq(STUDENTS.id, ATTENDANCE.studentId))
+    .where(
+      and(
+        eq(STUDENTS.grade, grade),
+        or(eq(ATTENDANCE.date, month), isNull(ATTENDANCE.date))
+      )
+    );
+
+  return NextResponse.json(result);
+}
