29e7f515c636ca14fc839e6e68a4e56661fc6476
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-12-20 00:52:32 +0530

    refactor: update style of searchBar

--- components/Students/StudentListTable/SearchBar.jsx
+++ components/Students/StudentListTable/SearchBar.jsx
@@ -2,7 +2,7 @@ import { IoSearch } from "react-icons/io5";
 
 const SearchBar = ({ value, onChange, placeholder = "Search..." }) => {
   return (
-    <div className="p-2 rounded-lg border shadow-sm flex gap-2 mb-4 max-w-sm items-center">
+    <div className="p-2 rounded-lg border shadow-sm flex gap-2 mb-4 max-w-sm items-center focus-within:border-blue-500 focus-within:shadow-blue-200 focus-within:shadow-md transition-all">
       <IoSearch className="text-gray-500" />
       <input
         type="text"
