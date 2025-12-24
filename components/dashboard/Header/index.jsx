5b6b0077d5cc6fa436c7c96a723c35b39efcf5c2
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-12-25 01:54:16 +0530

    refactor: extract user navigation into separate UserNav component

--- components/dashboard/Header/index.jsx
+++ components/dashboard/Header/index.jsx
@@ -1,24 +1,9 @@
-"use client";
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
-import Image from "next/image";
+import UserNav from "./UserNav";
 const Header = () => {
-  const { user } = useKindeBrowserClient();
-
-  const profileImageUrl = user?.picture;
   return (
     <div className="p-4 shadow-sm border flex justify-between">
       <div></div>
-      <div>
-        {profileImageUrl && (
-          <Image
-            src={profileImageUrl}
-            width={35}
-            height={35}
-            alt="user"
-            className="rounded-full"
-          />
-        )}
-      </div>
+      <UserNav />
     </div>
   );
 };
