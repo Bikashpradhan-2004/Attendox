ed642d6d3dc0e71b1bf86c51be1b975c9482e5d8
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-12-25 01:52:30 +0530

    feat: add logout confirmation dialog component

---
+++ components/dashboard/Header/LogoutConfirmDialog.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+const LogoutConfirmDialog = ({
+  onConfirm,
+  title = "Logout Confirmation",
+  description = "Are you sure you want to logout? You will need to sign in again to access your account.",
+  triggerButton,
+}) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{triggerButton}</AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Logout</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default LogoutConfirmDialog;
