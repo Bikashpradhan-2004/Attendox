0d89260dbad97867ac98f639a272621b1feea5b2
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-12-26 00:48:53 +0530

    feat(settings): implement logout confirmation dialog with router redirect

---
+++ app/dashboard/settings/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+import LogoutConfirmDialog from "@/components/Dashboard/Header/LogoutConfirmDialog";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+const Settings = () => {
+  const router = useRouter();
+  const handleLogout = () => {
+    router.push("/api/auth/logout");
+  };
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <LogoutConfirmDialog
+        onConfirm={handleLogout}
+        triggerButton={<Button className="shadow-lg shadow-blue-400/50">Signout</Button>}
+      />
+    </div>
+  );
+};  
+export default Settings;
