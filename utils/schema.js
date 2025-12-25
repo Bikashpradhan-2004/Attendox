60865ac6372848847138dc7aa96a3fa42016cbfb
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-12-26 00:44:29 +0530

    fix: increase the size of STUDENTS name in db (20 to 30 char)

--- utils/schema.js
+++ utils/schema.js
@@ -13,7 +13,7 @@ export const GRADES = pgTable("grades", {
 
 export const STUDENTS = pgTable("students", {
   id: serial("id").primaryKey(),
-  name: varchar("name", { length: 20 }).notNull(),
+  name: varchar("name", { length: 30 }).notNull(),
   grade: varchar("grade", { length: 10 }).notNull(),
   address: varchar("address", { length: 50 }),
   contact: varchar("contact", { length: 11 }),
