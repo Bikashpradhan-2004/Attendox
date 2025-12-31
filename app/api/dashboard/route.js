a7dcc64b47593ad45a546ecb7f6b2f20a3e80a68
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-12-31 11:28:00 +0530

    feat: add user-based attendance record for latest 7 days and handle 400/401 status responses

--- app/api/dashboard/route.js
+++ app/api/dashboard/route.js
@@ -2,26 +2,44 @@ import { db } from "@/utils";
 import { ATTENDANCE, STUDENTS } from "@/utils/schema";
 import { and, desc, eq, sql } from "drizzle-orm";
 import { NextResponse } from "next/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export async function GET(req) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user?.id) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
   const searchParams = req.nextUrl.searchParams;
   const date = searchParams.get("date");
   const grade = searchParams.get("grade");
-  console.log(date, grade);
+
+  if (!date || !grade) {
+    return NextResponse.json(
+      { error: "Date and grade are required" },
+      { status: 400 }
+    );
+  }
 
   const result = await db
     .select({
       day: ATTENDANCE.day,
-      presentCount: sql`count(${ATTENDANCE.day})`,
+      presentCount: sql`count(*)`.as("presentCount"),
     })
     .from(ATTENDANCE)
-    .leftJoin(
-      STUDENTS,
-      and(eq(ATTENDANCE.studentId, STUDENTS.id, eq(ATTENDANCE.date, date)))
+    .leftJoin(STUDENTS, eq(ATTENDANCE.studentId, STUDENTS.id))
+    .where(
+      and(
+        eq(STUDENTS.grade, grade),
+        eq(STUDENTS.userId, user.id),
+        eq(ATTENDANCE.date, date)
+      )
     )
     .groupBy(ATTENDANCE.day)
-    .where(eq(STUDENTS.grade, grade))
     .orderBy(desc(ATTENDANCE.day))
     .limit(7);
+
   return NextResponse.json(result);
 }
