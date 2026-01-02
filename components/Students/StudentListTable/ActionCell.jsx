b086c2c893c1b0cd26a52cdce8c8a670742f972f
Bikashpradhan-2004 <bkp96264@gmail.com>
2026-01-03 00:31:49 +0530

    feat(student): add edit functionality to ActionCell and update minWidth in DataTable

--- components/Students/StudentListTable/ActionCell.jsx
+++ components/Students/StudentListTable/ActionCell.jsx
@@ -1,17 +1,32 @@
-import { Trash2 } from "lucide-react";
+import { Trash2, Pencil } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import DeleteConfirmDialog from "./DeleteConfirmDialog";
 
-const ActionCell = ({ data, onDelete }) => {
+const ActionCell = ({ data, onEdit, onDelete }) => {
   return (
-    <DeleteConfirmDialog
-      onConfirm={() => onDelete(data.id)}
-      triggerButton={
-        <Button variant="destructive" size="icon">
-          <Trash2 size={18} />
-        </Button>
-      }
-    />
+    <div className="flex gap-4 items-center justify-center">
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => onEdit(data)}
+        className="hover:bg-blue-700 bg-blue-500 text-white hover:text-white"
+      >
+        <Pencil size={18} />
+      </Button>
+
+      <DeleteConfirmDialog
+        onConfirm={() => onDelete(data.id)}
+        triggerButton={
+          <Button
+            variant="destructive"
+            size="icon"
+            className="bg-red-500 hover:bg-red-700"
+          >
+            <Trash2 size={18} />
+          </Button>
+        }
+      />
+    </div>
   );
 };
 
